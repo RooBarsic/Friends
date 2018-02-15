41eadb2dc4630045e8cec03187356406ed9f02b3
RooBarsic <35538949+RooBarsic@users.noreply.github.com>
2018-02-15 21:29:12 +0300

    Create Kod1.cpp

---
+++ Kod1.cpp
@@ -0,0 +1,24 @@
+# include <bits/stdc++.h>
+# define sz(s) int(s.size())
+  using namespace std;
+  vector< string > isPalindrom(vector< string > word, int minLength)
+  {
+      vector< string > arr;
+
+
+
+
+      return arr;
+  }
+  int main()
+  {
+      vector< string > brr, crr;
+
+
+
+
+      crr = isPalindrom(brr, 6);
+      return 0;
+  }
+/**
+*/
