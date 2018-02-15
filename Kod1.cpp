a0abc13090d0aaf16b8eb413df38e6652c1fc034
RooBarsic <35538949+RooBarsic@users.noreply.github.com>
2018-02-15 22:01:06 +0300

    Update Kod1.cpp

--- Kod1.cpp
+++ Kod1.cpp
@@ -1,7 +1,7 @@
 # include <bits/stdc++.h>
 # define sz(s) int(s.size())
   using namespace std;
-  bool isPalindrom(string s;)
+  bool isPalindrom(string s)
   {
       int le = 0, ri = sz(s) - 1;
       while(le < ri){
@@ -10,6 +10,14 @@
         ri--;
       }
       return 1;
+  }
+  vector< string > PalindromFilter(vector< string > word, int minLength)
+  {
+      vector< string > ans;
+      for(int i = 0; i < sz(word); i++){
+        if((isPalindrom(word[i]) == 1) && (sz(word[i]) >= minLength)) ans.push_back(word[i]);
+      }
+      return ans;
   }
   int main()
   {
