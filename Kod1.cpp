c528f6901465996852f5ab4974720f7de0c306a7
RooBarsic <35538949+RooBarsic@users.noreply.github.com>
2018-02-19 21:27:04 +0300

    Update Kod1.cpp

--- Kod1.cpp
+++ Kod1.cpp
@@ -1,40 +1,31 @@
 # include <bits/stdc++.h>
 # define sz(s) int(s.size())
   using namespace std;
-  bool isPalindrom(string s)
-  {
-      int le = 0, ri = sz(s) - 1;
-      while(le < ri){
-        if(s[le] != s[ri]) return 0;
-        le++;
-        ri--;
-      }
-      return 1;
-  }
-  vector< string > PalindromFilter(vector< string > word, int minLength)
-  {
-      vector< string > ans;
-      for(int i = 0; i < sz(word); i++){
-        if((isPalindrom(word[i]) == 1) && (sz(word[i]) >= minLength)) ans.push_back(word[i]);
-      }
-      return ans;
+  int getpos(int x, int y){
+      if(x == y) return 0;
+      if(x > y) return 1;
+      if(x < y) return 2;
   }
   int main()
   {
-      int n, mLen;
-      vector< string > arr, brr;
-      cin >> n >> mLen;
-      for(int i = 1; i <= n; i++){
-        string s;
-        cin >> s;
-        arr.push_back(s);
+      long long n, ans, x, y, x2, x3, y2, y3;
+      string s;
+      cin >> n >> s;
+      x = y = 0;
+      x2 = y2 = x3 = y3 = 0;
+      ans = 0;
+      for(int i = 0; i < n; i++){
+        x3 = x2;
+        y3 = y2;
+        x2 = x;
+        y2 = y;
+        if(s[i] == 'U') y++;
+        else x++;
+        if(i >= 2){
+            if((getpos(x, y) == 1) && (getpos(x3, y3) == 2)) ans ++;
+            else if((getpos(x, y) == 2) && (getpos(x3, y3) == 1)) ans++;
+        }
       }
-      brr = PalindromFilter(arr, mLen);
-      for(int i = 0; i < sz(brr); i++){
-        cout << brr[i] << " ";
-      }
+      cout << ans;
       return 0;
   }
-/**
-http://abit.ifmo.ru/file/pages/74/Raspisanie_VI_2018.pdf
-*/
