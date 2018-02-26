431963a695904175117037e00ab3b22e72eadc7b
RooBarsic <35538949+RooBarsic@users.noreply.github.com>
2018-02-27 00:30:45 +0500

    Update Kod2.cpp

--- Kod2.cpp
+++ Kod2.cpp
@@ -1,42 +1,29 @@
-# include <iostream>
-# include <algorithm>
-  using namespace std;
-  int sum, kol;
-  long long arr[100100], brr[100100], crr[100100];
-  void count_kol(long long x){
-      int a;
-      kol = 0;
-      while(x > 0){
-        a = x % 10;
-        x = x / 10;
-        if(a % 2 != 0) kol++;
-      }
-  }
-  void count_sum(long long x){
-      int a;
-      sum = 0;
-      while(x > 0){
-        a = x % 10;
-        x = x / 10;
-        if(a % 2 != 0) sum += a;
-      }
-  }
-  int main()
-  {
-      long long n, k;
-      cin >> n;
-      for(int i = 1; i <= n; i++){
-        cin >> arr[i];
-        count_kol(arr[i]);
-        count_sum(arr[i]);
-        brr[i] = kol;
-        crr[i] = sum;
-      }
-      sort(brr + 1, brr + 1 + n);
-      sort(crr + 1, crr + 1 + n);
-      for(int i = 1; i <= n; i++){
-        cout << brr[i] << " " << crr[i] << " ";
-      }
-      return 0;
-  }
-гhttps://paste.ubuntu.com/p/q2r7rGbGjJ/
+#include <iostream>
+using namespace std;
+int main ()
+{
+    long long p, n, i, j, s = 0, t, r, h;
+    cin>>p>>n;
+    t = n;
+    for(t = n; t >=2; t --)
+    {
+        s = 0;
+        for(i = 2; i <= p; i++)
+        {
+            r = n/i;
+            for(j = 1; j <= r; j ++)
+            {
+                h = i * j;
+                if(h == t)
+                    s++;
+            }
+        }
+        if(s == 0)
+        {
+            cout<<t;
+            return 0;
+        }
+    }
+    cout<<"-1";
+    return 0;
+}
