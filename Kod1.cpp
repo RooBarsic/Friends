6a8d41f4440d6bd5e40943d48912243ed83c882d
RooBarsic <35538949+RooBarsic@users.noreply.github.com>
2018-03-06 21:54:49 +0500

    Update Kod1.cpp

--- Kod1.cpp
+++ Kod1.cpp
@@ -1,18 +1,23 @@
-# include <bits/stdc++.h>
+ # include <bits/stdc++.h>
 # define IoS ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 # define sz(s) int(s.size())
   using namespace std;
   int main()
   {
-    long long a, b, aa, bb;
-    cin >> a >> b;
-    while((a != 0) && (b != 0)){
-      aa = a; bb = b;
-      a = a % (2 * b);
-      if(a != 0) b = b % (2 * a);
-      if((a == aa) && (b == bb)) break;
-    }
-    cout << a << " " << b;
-      
-      return 0;
-  }
+     string s;
+     int f = 0:
+     cin >> s;
+     for(int b = 0, c=sz(s); b < c; b++){
+       if(int(s[b]) - int('a') <= f){
+         s[b] = char(int('a')+f);
+         f++;
+       }
+       if(f >= 26) break;
+     }
+     if(f < 26) cout<<"-1";
+     else cout << s;
+     return 0;
+   }
+
+
+  
