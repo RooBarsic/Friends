bf23436387573385ace255be1fd4da8231cba41c
RooBarsic <35538949+RooBarsic@users.noreply.github.com>
2018-03-08 16:36:24 +0300

    Update Kod2.cpp

--- Kod2.cpp
+++ Kod2.cpp
@@ -1,33 +1,7 @@
 
-#include <iostream>
+# include <bits/stdc++.h>
 using namespace std;
 int main ()
 {
-    long long p, n, i, j, s = 0, t, r, h;
-    cin>>p>>n;
-    t = n;
-    for(t = n; t >=2; t --)
-    {
-        s = 0;
-        for(i = 2; i <= p; i++)
-        {
-            r = n/i;
-            for(j = 1; j <= r; j ++)
-            {
-                h = i * j;
-                if(h == t){
-                    s++;
-                    break;
-                }
-            }
-            if(s != 0) break;
-        }
-        if(s == 0)
-        {
-            cout<<t;
-            return 0;
-        }
-    }
-    cout<<"-1";
     return 0;
 }
