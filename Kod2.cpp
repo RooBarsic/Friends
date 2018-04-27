7fa5f4222ce9a6e6c6f2367a1705658487d93e66
RooBarsic <35538949+RooBarsic@users.noreply.github.com>
2018-04-27 22:18:52 +0300

    Update Kod2.cpp

--- Kod2.cpp
+++ Kod2.cpp
@@ -1,7 +1,41 @@
-
 # include <bits/stdc++.h>
-using namespace std;
-int main ()
-{
-    return 0;
-}
+# define sz(s) int(s.size())
+  using namespace std;
+  int days[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+  vector< string > tasks[40];
+  int main()
+  {
+      long long n, x, m;
+      cin >> n;
+      m = 1;
+      for(int i = 1; i <= n; i++){
+        string s;
+        cin >> s;
+        if(s == "ADD"){
+            cin >> x >> s;
+            tasks[x].push_back(s);
+        }
+        else if(s == "NEXT"){
+            int nextm = (m % 12) + 1;
+            for(int d = days[nextm]; d > days[m]; d--){
+                tasks[d].clear();
+            }
+            for(int d = days[nextm] + 1; d <= days[m]; d++){
+                tasks[days[nextm]].insert(tasks[days[nextm]].end(), tasks[d].begin(), tasks[d].end());
+            }
+            m = nextm;
+        }
+        else {
+            cin >> x;
+            cout << tasks[x].size() << " ";
+            for(string task : tasks[x]){
+                cout << task << " ";
+            }
+            cout << '\n';
+        }
+      }
+      return 0;
+  }
+/**
+
+*/
