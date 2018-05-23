9b63b1547dd5bd59bbb93ef0960b80eb1a53eb4c
RooBarsic <35538949+RooBarsic@users.noreply.github.com>
2018-05-23 09:01:51 +0300

    Update Physology.java

--- Physology.java
+++ Physology.java
@@ -7,3 +7,19 @@ http://psytests.org/result?v=epiY4FkM
 
 Тест 2
   http://psytests.org/result?v=lnhA2MtR3f96
+
+
+
+
+Doklad
+http://psytests.org/result?v=mteA1i
+
+https://www.16personalities.com/ru/lichnost-infp
+
+https://www.16personalities.com/ru/lichnost-infp
+
+http://psytests.org/result?v=canA150VZVS2zHe
+
+http://psytests.org/result?v=lnhR1bN2bOKWQOao
+
+http://psytests.org/result?v=epiAPbr
