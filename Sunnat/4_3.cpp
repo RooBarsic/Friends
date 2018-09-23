3fbf9cdebf553736dddec0f2029bcc3404dc856d
RooBarsic <35538949+RooBarsic@users.noreply.github.com>
2018-09-23 21:13:59 +0300

    Create 4_3.cpp

---
+++ Sunnat/4_3.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+  using namespace std;
+  struct ochered{
+	  stack< int > stak1, stak2;
+	  void push_back(int a){
+		  stak1.push(a);
+	  }
+	  int pop_front(){
+		  if(stak1.size() == 0) return -1;
+		  while(stak1.size() > 1){
+			  int a = stak1.top();
+			  stak1.pop();
+			  stak2.push(a);
+		  }
+		  int ans = stak1.top();
+		  stak1.pop();
+		  while(stak2.size() > 0){
+			  int a = stak2.top();
+			  stak1.push(a);
+			  stak2.pop();
+		  }
+		  return ans;
+	  }
+  };
+  int main()
+  {
+	  ochered my_ochered;
+	  int n;
+	  cin >> n;
+	  for(int i = 1; i <= n; i++){
+		  int command, x;
+		  cin >> command >> x;
+		  if(command == 3) my_ochered.push_back(x);
+		  else if(command == 2){
+			  if(x != my_ochered.pop_front()){
+				  cout << "NO";
+				  return 0;
+			  }
+		  }
+	  }
+	  cout << "YES";
+	  return 0;
+  }
