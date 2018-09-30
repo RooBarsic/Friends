eb23ce060e2ae11c8c51173edc6d4e864a1c4deb
RooBarsic <35538949+RooBarsic@users.noreply.github.com>
2018-09-30 20:28:50 +0300

    Update 6_4.cpp

--- Sunnat/6_4.cpp
+++ Sunnat/6_4.cpp
@@ -1,14 +1,15 @@
 # include <bits/stdc++.h>
   using namespace std;
+// get_max - возврашает максимальный ( последний ) элемент из сета ( который мы ему даём )
   int get_max(multiset< int > st){
-      auto it = st.end();
-      it--;
-      return *(it);
+      auto it = st.end(); // берём указатель на конец сета ( не на последний элемент сета )
+      it--;               // т.к it - указывает на конец сета, то (it--) - будет указывать на последний элемент сета 
+      return *(it);       // т.к. it - это указатель, то *(it) - даёт нам число в сете на которое указывает it
   }
   int main()
   {
-      int arr[100000010];
-      multiset< int > st1;
+      int arr[100000010];     
+      multiset< int > st1;    // возьмём сет
       int n, k;
       cin >> n;
       for(int i = 0; i < n; i++){
