f3bcc1602e207088cdd82b60cbeff5f6ceb8fd08
Farrukh Karimov <35538949+RooBarsic@users.noreply.github.com>
2019-04-12 08:41:47 +0300

    Update lab_2_task_1.cpp

--- Ilhom/lab_2_task_1.cpp
+++ Ilhom/lab_2_task_1.cpp
@@ -1,10 +1,37 @@
-// partial solution
+// full solution
 # include <bits/stdc++.h>
+#define MAX 100
 using namespace std;
 double countVolume(double Radius, double Height){
 	double pi = 3.14l
 	double V = (pi * Radius * Radius) * Height;
 	return V;
+}
+void printWords(int id, char s[]){ // выделяем слова в строке и выводим их, рекурсивно, в обратном порядке
+	int k = int(strlen(s));
+	if(id < k){
+		string word = "";
+		for(int i = id; i < k; i++){
+			if(s[i] == ','){
+				printWords(i + 1, s);
+				cout << word << " ";
+				return;
+			}
+			else word += s[i];
+		}
+		cout << word << " ";
+	}
+}
+char* getString(){ // берём строку из stdin
+    char *str = (char*) malloc(MAX * sizeof(char));
+    cin >> str;
+    return str;
+}
+void printString(char s[]){ // чтобы вывести строку
+    int k = strlen(s);
+    for(int i = 0; i < k; i++){
+        cout << s[i];
+    }
 }
 int main(){
 	double radius, height;
@@ -13,5 +40,11 @@ int main(){
 	cout << " print Height : ";
 	cin >> height;
 	cout << " Cylinder volume = " << countVolume(radius, height);
+	
+	// test string
+	char *crr = getString();
+        cout << "for char, result : ";
+	printWords(0, crr);
+	cout << '\n';
 	return 0;
 }
