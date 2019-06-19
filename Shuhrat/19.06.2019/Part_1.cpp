b68223545d79d5b10aae9852b8806b75f2b30ace
Farrukh Karimov <35538949+RooBarsic@users.noreply.github.com>
2019-06-19 19:55:05 +0300

    Update Part_1.cpp

--- Shuhrat/19.06.2019/Part_1.cpp
+++ Shuhrat/19.06.2019/Part_1.cpp
@@ -2,37 +2,37 @@
   using namespace std;
   
   struct PlanetInfo {
-      string name;
-      int diameter;
-      bool haslife;
-      int moons;
+      string name;   // имя планеты
+      int diameter;  // диаметр планеты
+      bool haslife; // есть ли жизнь на этой планете ( да/нет )
+      int moons;   // количество спутников планеты
   };
   
   class Planet {
       public : 
-        Planet(){
+        Planet(){   // конструктор без параметров
             infoAboutPlanet.name = "";
             infoAboutPlanet.diameter = 0;
             infoAboutPlanet.haslife = 0;
             infoAboutPlanet.moons = 0;
         }
         
-        Planet(PlanetInfo info){
+        Planet(PlanetInfo info){   // конструктор который принимает обект PlanetInfo
             infoAboutPlanet = info;
         }
         
-        Planet(string name, int diameter, bool haslife, int moons){
+        Planet(string name, int diameter, bool haslife, int moons){    // конструктор с параметрами Имя, Диаметр, Есть ли жизнь, количество спутников 
             infoAboutPlanet.name = name;
             infoAboutPlanet.diameter = diameter;
             infoAboutPlanet.haslife = haslife;
             infoAboutPlanet.moons = moons;
         }
         
-        ~Planet(){
+        ~Planet(){     // деструктор
             
         }
         
-        void getPlanet(){
+        void getPlanet(){    // функция для ввода данных о планете
             cout << "Введите имя планеты : ";
             cin >> infoAboutPlanet.name;
             cout << "Введите диаметр планеты : ";
@@ -43,7 +43,7 @@
             cin >> infoAboutPlanet.moons;
         }
         
-        void print(){
+        void print(){    // функция для вывода данных о планете
             cout << "Информация о планете { ";
             cout << " Имя: " << infoAboutPlanet.name << "; "
                  << " Диаметр: " << infoAboutPlanet.diameter << "; "
@@ -52,19 +52,20 @@
                  << " } " << '\n';
         }
         
+	  // перегружаем оператор < . Чтобы сравнение < - происходило так-как мы хотим
         friend bool operator<(const Planet& left, const Planet& right){
-			if(left.infoAboutPlanet.diameter < right.infoAboutPlanet.diameter) return true;
-			else if(left.infoAboutPlanet.diameter == right.infoAboutPlanet.diameter){
-				if(left.infoAboutPlanet.moons < right.infoAboutPlanet.moons) return true;
-				else if(left.infoAboutPlanet.moons == right.infoAboutPlanet.moons){
-					if(left.infoAboutPlanet.name < right.infoAboutPlanet.name) return true;
+			if(left.infoAboutPlanet.diameter < right.infoAboutPlanet.diameter) return true; // сравниваем диаметры
+			else if(left.infoAboutPlanet.diameter == right.infoAboutPlanet.diameter){   // если диаметры равны
+				if(left.infoAboutPlanet.moons < right.infoAboutPlanet.moons) return true;  // сравниваем количество спутников
+				else if(left.infoAboutPlanet.moons == right.infoAboutPlanet.moons){  // если количество спутников тоже равны
+					if(left.infoAboutPlanet.name < right.infoAboutPlanet.name) return true;  // сравниваем названия планет
 				}
 			}
 			return false;
 		}
       
       private :
-        PlanetInfo infoAboutPlanet;
+        PlanetInfo infoAboutPlanet;   // тут храним информацию о планете
   };
   
   int main()
@@ -78,13 +79,16 @@
           planets[i].getPlanet();
           cout << '\n';
       }
+	  
       cout << '\n' << "----------- Выводим данные о планетах ------------ " << '\n';
       for(int i = 1; i <= numberOfPlanets; i++){
-		  cout << " Планета № " << i << ' ';
-		  planets[i].print();
-	  }
+	  cout << " Планета № " << i << ' ';
+	  planets[i].print();
+      }
+	  
       cout << '\n' << "----------- Сортируем данные о планетах ------------ " << '\n';
       sort(planets + 1, planets + 1 + numberOfPlanets);
+	  
       cout << '\n' << "----------- Выводим данные о планетах - после сортировки ------------ " << '\n';
       for(int i = 1; i <= numberOfPlanets; i++){
 		  cout << " Планета № " << i << ' ';
