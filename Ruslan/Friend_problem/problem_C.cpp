e3fa6811d576974cd6fad7c8f38d3ea34a3a9713
Farrukh Karimov <35538949+RooBarsic@users.noreply.github.com>
2019-06-30 20:44:31 +0300

    Update problem_C.cpp

--- Ruslan/Friend_problem/problem_C.cpp
+++ Ruslan/Friend_problem/problem_C.cpp
@@ -1,22 +1,24 @@
 # include<bits/stdc++.h>
 # define sz(s) int(s.size())
 using namespace std;
-int prost = 500009;
-int calc_hash(string s){
+int mood = 500000;
+int get_hash1(string s){
+    long long prost = 500009ll;
     if(sz(s) == 0) return 0;
-    int hash = 0, i;
+    long long hash = 0ll;
     for(int i = 0, len = sz(s); i < len; i++) {
-        hash = (prost * hash) + int(s[i]);
+        hash = ((prost * 1ll * hash)%mood + abs(int(s[i]))) % mood;
     }
     return hash;
 }
 int arr[505000];
 int main(){
+    int n;
     cin >> n;
     for(int i = 1; i <= n; i++){
         string s;
         cin >> s;
-        arr[i] = calc_hash(s);
+        arr[i] = get_hash1(s);
     }
     sort(arr + 1, arr + 1 + n);
     int ans = 1;
