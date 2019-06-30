dfaf9f77b6f236d0dc5050e18f3fe90098adac37
Farrukh Karimov <35538949+RooBarsic@users.noreply.github.com>
2019-07-01 00:32:04 +0300

    Create file2.cpp

---
+++ Ruslan/Friend_problem/ProblemB/file2.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    srand(time(NULL));
+    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+    int n;
+    cin >> n;
+    double right[n], pred[n];
+    double answer, auc = 0.0, count = 0.0;
+   
+    for(int i = 1; i <= n; i++){
+        cin >> right[i] >> pred[i];
+    }
+   
+    for (int i = 1; i <= n; i++){
+        for (int j = i; i <= n; i++){
+            if(right[i] > right[j]){
+                count += 1.0;
+                if(pred[i] > pred[j])
+                    auc += 1.0;
+                if(pred[i] == pred[j])
+                    auc += 0.5;
+            } else if(right[i] < right[j]) {
+                count += 1.0;
+                if(pred[i] < pred[j])
+                    auc += 1.0;
+                if(pred[i] == pred[j])
+                    auc += 0.5;
+            }
+        }
+    }
+   
+    answer = auc / count;
+    cout << fixed << setprecision(9) << answer;
+   
+    return 0;
+ }
