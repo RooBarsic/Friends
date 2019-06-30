6d52f7a634bf3ff17eaacd22a8da50e588120aa6
Farrukh Karimov <35538949+RooBarsic@users.noreply.github.com>
2019-07-01 00:12:40 +0300

    Create file1.cpp

---
+++ Ruslan/Friend_problem/ProblemB/file1.cpp
@@ -0,0 +1,13 @@
+# include<bits/stdc++.h>
+# define sz(s) int(s.size())
+using namespace std;double arr[100100], brr[100100];
+int main(){
+    srand(time(NULL));
+    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        cin >> arr[i] >> brr[i];
+    }
+    
+ }
