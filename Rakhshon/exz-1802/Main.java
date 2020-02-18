ecc7ce828a305746c117776367e9a7ba0b7c60d8
Farrukh Karimov <35538949+RooBarsic@users.noreply.github.com>
2020-02-18 17:50:08 +0300

    Create Main.java

---
+++ Rakhshon/exz-1802/Main.java
@@ -0,0 +1,68 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        String option, fileName = null;
+        if(args.length == 0){
+            System.out.println(" Error. No option was found ");
+            return;
+        } else if(args.length == 1){
+            option = args[0];
+        } else if(args.length == 2){
+            option = args[0];
+            fileName = args[1];
+        } else if(args.length > 2) {
+            System.out.println(" Error. Too many options are given. ");
+            return;
+        }
+
+    }
+
+    public static void execute(final String option, final String fileName){
+        List<String> inputData = getData(fileName);
+        switch(option){
+            case "--ignore-leading-blanks" :
+                break;
+            case "--dictionary-order" :
+                break;
+            case "--ignore-case" :
+                break;
+            case "--ignore-nonprinting":
+                break;
+            case "--numeric-sort" :
+                break;
+            case "--reverse" :
+                break;
+            default:
+                System.out.println(" Error. Wrong option! ");
+        }
+    }
+
+    public static List<String> getData(final String fileName){
+        final List<String> inputData = new ArrayList<>();
+        Scanner scanner = null;
+        try {
+            if (fileName == null) {
+                scanner = new Scanner(System.in);
+            } else {
+                scanner = new Scanner(new File(fileName));
+            }
+            while (scanner.hasNext()) {
+                final String inputLine = scanner.nextLine();
+                inputData.add(inputLine);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            if(scanner != null){
+                scanner.close();
+            }
+            return inputData;
+        }
+    }
+}
