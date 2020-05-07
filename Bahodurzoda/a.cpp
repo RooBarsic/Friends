3fd27e05abf51c475a2a8072b75f6a3a15ea1dd1
Farrukh Karimov <35538949+RooBarsic@users.noreply.github.com>
2020-05-07 19:07:29 +0300

    Update a.cpp

--- Bahodurzoda/a.cpp
+++ Bahodurzoda/a.cpp
@@ -1,7 +1,7 @@
 
 	if(ui->radioButton->isChecked() == true){
 		vector<double> buffer;
-	    buffer.resize(101, 0);
+		buffer.resize(101, 0);
 
 		for(int i = 0; i < 5; i++){
 			result.temperature[i].resize(101, 0);
@@ -10,13 +10,13 @@
 		int errorCode;
 		if ((errorCode = MPI_Init(0, 0)) != 0){
 			cout << "Ошибка запуска, выполнение остановлено " << endl;
-	        MPI_Abort(MPI_COMM_WORLD, errorCode);
+			MPI_Abort(MPI_COMM_WORLD, errorCode);
 			return errorCode;
 		}
 		int myid;
-	    MPI_Comm_rank(MPI_COMM_WORLD,&myid);  //  получаем в myid - id нащего процесса
+		MPI_Comm_rank(MPI_COMM_WORLD,&myid);  //  получаем в myid - id нащего процесса
 
-	    if(myid == 0){
+		if(myid == 0){
 			for(int i = 0; i < 51; i++){
 				buffer[i] = 2. * Umax * dx * i;
 			}
@@ -27,5 +27,5 @@
 		}
 
 		MPI_Reduce(&(*buffer.begin()), &(*result.temperature[0].begin()), 101,  MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-	    MPI_Finalize();
+		MPI_Finalize();
 	}
